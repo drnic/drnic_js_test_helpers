70b7d8c86aea35f0932283405cce0f2b96279c3d
Dr Nic <drnicwilliams@gmail.com>
2008-02-14 15:00:54 +1000

    Tests for failing ajax

---
+++ src/ajax_mock.js
@@ -0,0 +1,8 @@
+Ajax.Request.prototype.request = function(url) {
+  var response = new Ajax.Response(this);
+  var request  = this;
+  Ajax.Request.MockedRequests.each(function(mock) {
+    if (url == mock[0]) mock[1](request, response);
+  });
+  // this.options.onComplete(response);
+};
