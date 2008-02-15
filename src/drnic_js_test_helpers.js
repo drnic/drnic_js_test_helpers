c74dbb3ca1e9c862a40fdd26c548789fd0745350
Dr Nic <drnicwilliams@gmail.com>
2008-02-16 08:45:56 +1000

    Added assertions.js to distribution (oops)

--- src/drnic_js_test_helpers.js
+++ src/drnic_js_test_helpers.js
@@ -5,4 +5,4 @@ DrNic.JsTestHelpers = {
   Version: '<%= APP_VERSION %>',
 };
 
-<%= include 'simulate_events.js', 'ajax_mock.js' %>
+<%= include 'simulate_events.js', 'ajax_mock.js', 'assertions.js' %>
