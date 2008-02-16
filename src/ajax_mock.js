c75d997f8ea924b47f07132cae7a7670d927c9fd
Dr Nic <drnicwilliams@gmail.com>
2008-02-17 09:12:19 +1000

    moved tests to test/unit + updated rake tasks

--- src/ajax_mock.js
+++ src/ajax_mock.js
@@ -13,22 +13,28 @@ Ajax.Request.setupMock = function(url, block) {
 
 Ajax.Request.clearMocks = function() {
   Ajax.Request.MockedRequests = $H();
+  if (Ajax.Request.prototype.requestOrig) {
+    Ajax.Request.prototype.request = Ajax.Request.prototype.requestOrig;
+    Ajax.Request.prototype.requestOrig = null;
+  }
 };
 
 Ajax.Request.clearMocks();
 
-Ajax.Request.prototype.requestOrig = Ajax.Request.prototype.request;
-Ajax.Request.prototype.request = function(url) {
-  var response = new Ajax.Response(this);
-  var request  = this;
-  var found    = false;
-  Ajax.Request.MockedRequests.each(function(mock) {
-    if (!found && url == mock[0]) {
-      mock[1](request, response);
-      found = true;
+Ajax.Request.prepareMocks = function() {
+  Ajax.Request.prototype.requestOrig = Ajax.Request.prototype.request;
+  Ajax.Request.prototype.request = function(url) {
+    var response = new Ajax.Response(this);
+    var request  = this;
+    var found    = false;
+    Ajax.Request.MockedRequests.each(function(mock) {
+      if (!found && url == mock[0]) {
+        mock[1](request, response);
+        found = true;
+      }
+    });
+    if (!found) {
+      return this.requestOrig(url);
     }
-  });
-  if (!found) {
-    return this.requestOrig(url);
-  }
+  };
 };
