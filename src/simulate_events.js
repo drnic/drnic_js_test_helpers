22fe34b655e57bb6bec6d95f1231ec3bdbb0e3af
Dr Nic <drnicwilliams@gmail.com>
2008-02-17 14:31:32 +1000

    imported Event.simulate from http://code.google.com/p/protolicious/source/browse/trunk/

--- src/simulate_events.js
+++ src/simulate_events.js
@@ -1,44 +1,49 @@
-// from http://blog.pothoven.net/2007/08/synthesizing-events-in-javascript.html
+// from http://code.google.com/p/protolicious/source/browse/trunk/event.simulate.js
 Event.simulate = function(element, eventName) {
-  var event;
-  targetElement = $(element);
   
-  if (targetElement) {
-   // check for IE
-   if (window.ActiveXObject) {
-       event = document.createEventObject();
-       targetElement.fireEvent("on"+eventName,event);
-   } else {
-       switch (eventName) {
-           case "abort":
-           case "blur":
-           case "change":
-           case "error":
-           case "focus":
-           case "load":
-           case "reset":
-           case "resize":
-           case "scroll":
-           case "select":
-           case "submit":
-           case "unload":
-               event = document.createEvent("HTMLEvents");
-               event.initEvent(eventName, "true", "true");
-               break;
-           case "click":
-           case "dblclick":
-           case "mousedown":
-           case "mousemove":
-           case "mouseout":
-           case "mouseover":
-           case "mouseup":
-           case "contextmenu":
-               event = document.createEvent("MouseEvents");
-               event.initMouseEvent(eventName, true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
-               break;
-       }
-       targetElement.dispatchEvent(event);
-     }
+  var options = Object.extend({
+    pointerX: 0,
+    pointerY: 0,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true
+  }, arguments[2] || { } );
+  
+  var eventMatchers = {
+    'HTMLEvents': /load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll/,
+    'MouseEvents': /click|mousedown|mouseup|mouseover|mousemove|mouseout/
+  };
+  
+  var oEvent, eventType = null;
+  
+  for (var name in eventMatchers) {
+    if (eventMatchers[name].test(eventName)) eventType = name;
+  }
+  
+  if (!eventType) throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported; ' +
+    eventName + ' not supported');
+
+  if (document.createEvent) {
+    oEvent = document.createEvent(eventType);
+    if (eventType == 'HTMLEvents') {
+      oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+    }
+    else {
+      oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView, 
+        options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+        options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, $(element));
+    }
+    $(element).dispatchEvent(oEvent);
+  }
+  else {
+    options.clientX = options.pointerX;
+    options.clientY = options.pointerY;
+    oEvent = Object.extend(document.createEventObject(), options);
+    $(element).fireEvent('on' + eventName, oEvent);
   }
 }
 
