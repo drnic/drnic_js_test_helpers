0dd7995f35ed474fe3a727eb248954c148557e3c
Dr Nic <drnicwilliams@gmail.com>
2008-02-18 14:39:24 +1000

    removed messages from assertDifference

--- src/assertions.js
+++ src/assertions.js
@@ -1,12 +1,10 @@
 Test.Unit.Testcase.prototype.assertDifference = function(expr, fn, count) {
-  var message = arguments[3] || 'Should be equal';
   var orig = eval(expr);
   fn();
   var after = eval(expr);
-  this.assertEqual(orig + count, after, message);
+  this.assertEqual(orig + count, after);
 };
 
 Test.Unit.Testcase.prototype.assertNoDifference = function(expr, fn) {
-  var message = arguments[2] || 'Should be equal';
-  this.assertDifference(expr, fn, 0, message);
+  this.assertDifference(expr, fn, 0);
 };
