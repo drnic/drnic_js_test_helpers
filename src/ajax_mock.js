58adf44a0385ca155b0a6c0bbc6f094b3420291c
Dr Nic <drnicwilliams@gmail.com>
2008-02-18 14:53:05 +1000

    only load ajax_mock if Prototype installed

--- src/ajax_mock.js
+++ src/ajax_mock.js
@@ -5,38 +5,37 @@
 //   response.responseJSON = "data";
 //   request.options.onComplete(response);
 // });
+if (typeof Prototype != "undefined") {
 
-var Ajax = Ajax || {};
-Ajax.Request = Ajax.Request || {};
+  Ajax.Request.setupMock = function(url, block) {
+    Ajax.Request.MockedRequests.set(url, block);
+  };
 
-Ajax.Request.setupMock = function(url, block) {
-  Ajax.Request.MockedRequests.set(url, block);
-};
-
-Ajax.Request.clearMocks = function() {
-  Ajax.Request.MockedRequests = $H();
-  if (Ajax.Request.prototype.requestOrig) {
-    Ajax.Request.prototype.request = Ajax.Request.prototype.requestOrig;
-    Ajax.Request.prototype.requestOrig = null;
-  }
-};
-
-Ajax.Request.clearMocks();
-
-Ajax.Request.prepareMocks = function() {
-  Ajax.Request.prototype.requestOrig = Ajax.Request.prototype.request;
-  Ajax.Request.prototype.request = function(url) {
-    var response = new Ajax.Response(this);
-    var request  = this;
-    var found    = false;
-    Ajax.Request.MockedRequests.each(function(mock) {
-      if (!found && url == mock[0]) {
-        mock[1](request, response);
-        found = true;
-      }
-    });
-    if (!found) {
-      return this.requestOrig(url);
+  Ajax.Request.clearMocks = function() {
+    Ajax.Request.MockedRequests = $H();
+    if (Ajax.Request.prototype.requestOrig) {
+      Ajax.Request.prototype.request = Ajax.Request.prototype.requestOrig;
+      Ajax.Request.prototype.requestOrig = null;
     }
   };
-};
+
+  Ajax.Request.clearMocks();
+
+  Ajax.Request.prepareMocks = function() {
+    Ajax.Request.prototype.requestOrig = Ajax.Request.prototype.request;
+    Ajax.Request.prototype.request = function(url) {
+      var response = new Ajax.Response(this);
+      var request  = this;
+      var found    = false;
+      Ajax.Request.MockedRequests.each(function(mock) {
+        if (!found && url == mock[0]) {
+          mock[1](request, response);
+          found = true;
+        }
+      });
+      if (!found) {
+        return this.requestOrig(url);
+      }
+    };
+  };
+}
