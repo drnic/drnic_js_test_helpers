ef733f9ac8e1869021bd5d47d0e498e06e010dbb
Dr Nic <drnicwilliams@gmail.com>
2008-02-18 22:08:16 +1000

    added assertTagDifference

--- src/assertions.js
+++ src/assertions.js
@@ -8,3 +8,11 @@ Test.Unit.Testcase.prototype.assertDifference = function(expr, fn, count) {
 Test.Unit.Testcase.prototype.assertNoDifference = function(expr, fn) {
   this.assertDifference(expr, fn, 0);
 };
+
+Test.Unit.Testcase.prototype.assertTagDifference = function(selector, fn, count) {
+  var expr = "document.getElementsByTagName('" + selector + "').length";
+  var orig = eval(expr);
+  fn();
+  var after = eval(expr);
+  this.assertEqual(orig + count, after);
+};
